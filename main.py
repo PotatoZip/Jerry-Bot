c9c39743256919c6a9bd9bc703d14e41798beceb
Wiktor <wiktor.zmiendak@gmail.com>
2023-12-03 12:51:52 +0100

    Add Logging setup

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import settings
+import logging
 import discord
 from discord.ext import commands
+
+logger = settings.logging.getLogger("bot")
 
 def run():
     intents = discord.Intents.default()
     bot = commands.Bot(command_prefix="!", intents=intents)
     @bot.event
     async def on_ready():
-        print(bot.user)
-        print(bot.user.id)
-        print("____________")
-    bot.run(settings.DISCORD_API_SECRET)
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
     run()
